eede02d3993698a8ee280848acf4384e434b354f
brbrbr <1730268341@qq.com>
2025-05-20 15:12:12 +0800

    Update ljy.js

--- js/ljy.js
+++ js/ljy.js
@@ -8,15 +8,17 @@ var vue = new Vue({
             player: "",
             kv:
             {
-                "f8c5167e6dad7a6d": { title: "打僵尸（增减门玩法）", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763445/vthumb/c347751917fbfb30d8f34a80568ddd98.4.jpg/144p.jpg", duration: "00:26" },
-                "6a38bd6316fb903a": { title: "黑洞吞噬玩法", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763734/vthumb/bd4105928c5ba2e2617440befba2cd62.4.jpg/144p.jpg", duration: "00:42" },
+                "f8c5167e6dad7a6d": { title: "打僵尸（增减门）", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763445/vthumb/c347751917fbfb30d8f34a80568ddd98.4.jpg/144p.jpg", duration: "00:26" },
+                "6a38bd6316fb903a": { title: "黑洞吞噬", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763734/vthumb/bd4105928c5ba2e2617440befba2cd62.4.jpg/144p.jpg", duration: "00:42" },
                 "66a6555f95e61916": { title: "智慧城市", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763545/vthumb/96ce14a4168fa249f1c133f362726759.4.jpg/144p.jpg", duration: "01:30" },
                 "ae0e461fbbf7a3fa": { title: "汽车展示", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763577/vthumb/eeccb197f06ab2412886cefb7a53068e.4.jpg/144p.jpg", duration: "02:06" },
                 "163c8c87eab42367": { title: "2D打老鼠", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763553/vthumb/82f49281b173f4795f7b894d428f693a.4.jpg/144p.jpg", duration: "00:53" },
                 "e84f2060d0488757": { title: "多人联网游戏", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2697038/vthumb/fbff0183da77c301949ab7e5217cd68d.4.jpg/144p.jpg", duration: "04:56" },
                 "405e91a5c29f4fdb": { title: "多线程通信", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2696577/vthumb/7f9b96efcec9f8f865fba13628ece8e1.5.jpg/144p.jpg", duration: "00:21" },
+                "b8e08f0e47553d5a": { title: "消灭星星(点消)", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2775316/vthumb/b2bb25c50c4ada73c992490136cf8b4a.4.jpg/144p.jpg", duration: "00:26" },
+                "af88d7ceddff6886": { title: "塔防游戏", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2775317/vthumb/a1d6d18ddacc32ab9ce2f043c088e30c.4.jpg/144p.jpg", duration: "00:17" },
+
                 "0000000000000000": { title: "纸牌匹配", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
-                "0000000000000001": { title: "消灭星星(点消)", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
                 "0000000000000002": { title: "消消乐(Match 3)", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
                 "0000000000000003": { title: "消消乐结合SLG", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
                 "0000000000000004": { title: "合成小游戏", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
@@ -28,31 +30,19 @@ var vue = new Vue({
                 "0000000000000010": { title: "割草闯关", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
                 "0000000000000011": { title: "魂类游戏", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
                 "0000000000000012": { title: "ARPG游戏", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
-                "0000000000000013": { title: "塔防游戏", description: "这个人很懒，什么都没留下。", src: "", duration: "01:23" },
             }
             ,
             categorys: [
                 {
                     name: "游戏",
                     players: [
+                        "af88d7ceddff6886",
                         "f8c5167e6dad7a6d",
                         "6a38bd6316fb903a",
+                        "b8e08f0e47553d5a",
                         "163c8c87eab42367",
                         "e84f2060d0488757",
-                        "0000000000000000",
-                        "0000000000000001",
-                        "0000000000000002",
-                        "0000000000000003",
-                        "0000000000000004",
-                        "0000000000000005",
-                        "0000000000000006",
-                        "0000000000000007",
-                        "0000000000000008",
-                        "0000000000000009",
-                        "0000000000000010",
-                        "0000000000000011",
-                        "0000000000000012",
-                        "0000000000000013"
+
 
                     ]
 

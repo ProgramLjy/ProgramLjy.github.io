940f50d0156a1f9ea8403f57d5a84f0cff8720c2
brbrbr <1730268341@qq.com>
2025-07-02 00:39:28 +0800

    Update ljy.js

--- js/ljy.js
+++ js/ljy.js
@@ -6,7 +6,7 @@ var vue = new Vue({
             aboutDialogVisible: false,
             dialogPlayerVisible: false,
             player: "",
-            auto:false,
+            auto: false,
             kv:
             {
                 "f8c5167e6dad7a6d": { title: "增减门", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2763445/vthumb/c347751917fbfb30d8f34a80568ddd98.6_69.jpg/144p.jpg", duration: "00:26" },
@@ -23,27 +23,38 @@ var vue = new Vue({
                 "2894bd754370c7f7": { title: "挖地道", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2784793/vthumb/3b23c8dc905da9aeab232a6d87e60694.10_02.jpg/144p.jpg", duration: "00:27" },
                 "36d7de23266ad045": { title: "停车小游戏", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2790642/vthumb/a2eccda692746200d229b9a03dc60c5b.4.jpg/144p.jpg", duration: "00:57" },
 
-                "b0bdbebfcd16e8ed": { title: "丧尸集群练习", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2919483/vthumb/d76d09797f15a53a87a0a48f70010102.4.jpg/144p.jpg", duration: "00:34" },
+                "b0bdbebfcd16e8ed": { title: "群集练习", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2919483/vthumb/d76d09797f15a53a87a0a48f70010102.4.jpg/144p.jpg", duration: "00:34" },
                 "4bda6c5e7b143c07": { title: "武士冲锋", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2919486/vthumb/1c6fa611ba5e1ed2fa863881cab9f3a3.4.jpg/144p.jpg", duration: "00:19" },
+
+                "8adbe69435a84167": { title: "SLG与三消结合", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2920836/vthumb/45802a59a8c7af327c61758731ab9e1e.4.jpg/144p.jpg", duration: "01:10" },
+                "7888808a8a76ac3a": { title: "抖音弹幕爬取", description: "这个人很懒，什么都没留下。", src: "https://img.dogevideo.com/vcloud/12923/v/2920837/vthumb/358eafe1c4e7f979c259506653c3db01.1.jpg/144p.jpg", duration: "00:19" },
             }
             ,
             categorys: [
                 {
                     name: "游戏",
                     players: [
-                        "af88d7ceddff6886",
                         "f8c5167e6dad7a6d",
-                        "6a38bd6316fb903a",
-                        "b8e08f0e47553d5a",
-                        "5b0316556921e262",
-                        "3498498afb020fa2",
-                        "2894bd754370c7f7",
-                        "36d7de23266ad045",
-                        "b0bdbebfcd16e8ed",
                         "4bda6c5e7b143c07",
+                        "2894bd754370c7f7",
+                        "5b0316556921e262",
+                        "8adbe69435a84167",
+                        "6a38bd6316fb903a",
+                        "b0bdbebfcd16e8ed",
+                        "b8e08f0e47553d5a",
+                        "af88d7ceddff6886",
+                        "3498498afb020fa2",
+                        "36d7de23266ad045",
                         "163c8c87eab42367",
-                        "e84f2060d0488757",
+                        "e84f2060d0488757"
+                    ]
 
+                },
+                {
+                    name: "C++",
+                    players: [
+                        "7888808a8a76ac3a",
+                        "405e91a5c29f4fdb"
                     ]
 
                 },
@@ -51,17 +62,11 @@ var vue = new Vue({
                     name: "数字孪生",
                     players: [
                         "66a6555f95e61916",
-                        "ae0e461fbbf7a3fa",
+                        "ae0e461fbbf7a3fa"
                     ]
 
                 },
-                {
-                    name: "其他",
-                    players: [
-                        "405e91a5c29f4fdb",
-                    ]
 
-                },
             ],
             recommend: [
                 "f8c5167e6dad7a6d",
@@ -84,7 +89,7 @@ var vue = new Vue({
                     autoPlay: false,
                     logo: {
                         gravity: 'NorthWest',
-                        img: 'https://img-uc.dogecloud.com/12923/image/1747725869/dd9b399427806e354a69bc836c7cfc98.png?sign=1747738801-4ab921a1a62ce2dacd10637cdd977328-12923-7d0cec1874f322d05fba28199f84f124'
+                        img: 'https://img-uc.dogecloud.com/12923/image/1747725869/dd9b399427806e354a69bc836c7cfc98.png?sign=1751400001-688795e09a7fc5d1c958b19a8fb7b742-12923-6116cbc6daac3511ec20647d9b957eae'
                     },
                 });
                 vue.player.on('ready', function (p) {
